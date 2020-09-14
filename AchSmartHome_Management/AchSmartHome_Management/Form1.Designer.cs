4f9bd759e50a9eaa46e31970b14560471fd1d53f
Андрей <50486086+DarkCat09@users.noreply.github.com>
2020-09-14 20:35:24 +0400

    Add files via upload

---
+++ AchSmartHome_Management/AchSmartHome_Management/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace AchSmartHome_Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.button1 = new System.Windows.Forms.Button();
+            this.панельToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.соединитьсяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.панельToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(624, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(549, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Tag = "Login";
+            this.button1.Text = "Вход";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // панельToolStripMenuItem
+            // 
+            this.панельToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.соединитьсяToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.панельToolStripMenuItem.Name = "панельToolStripMenuItem";
+            this.панельToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.панельToolStripMenuItem.Text = "Панель";
+            // 
+            // соединитьсяToolStripMenuItem
+            // 
+            this.соединитьсяToolStripMenuItem.Name = "соединитьсяToolStripMenuItem";
+            this.соединитьсяToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.соединитьсяToolStripMenuItem.Text = "Подключиться";
+            this.соединитьсяToolStripMenuItem.Click += new System.EventHandler(this.соединитьсяToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 322);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Tag = "AchShTitle";
+            this.Text = "Управление умным домом AchSmartHome";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button button1;
+        private ControlPanel controlPanel1;
+        private System.Windows.Forms.ToolStripMenuItem панельToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem соединитьсяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ AchSmartHome_Management/AchSmartHome_Management/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AchSmartHome_Management
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ControlPanel controlPanel1 = new ControlPanel();
+            controlPanel1.Location = new System.Drawing.Point(0, 24);
+            Controls.Add(controlPanel1);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void соединитьсяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ConnectForm cf = new ConnectForm();
+            cf.ShowDialog();
+        }
+    }
+}
